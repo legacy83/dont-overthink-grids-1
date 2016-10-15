8e2e30dd0a89d44980d1e9cf9e381256371b020b
Thiago Senna <trsenna@gmail.com>
2016-10-15 20:11:24 -0300

    added some more content

--- front-page.php
+++ front-page.php
@@ -5,10 +5,10 @@
 
             <?php get_template_part( 'components/page/content', 'front-page-intro' ); ?>
             <?php get_template_part( 'components/page/content', 'front-page-2cols' ); ?>
-            <?php get_template_part( 'components/page/content', 'front-page-3cols' ); ?>
-            <?php get_template_part( 'components/page/content', 'front-page-4cols' ); ?>
             <?php get_template_part( 'components/page/content', 'front-page-2cols-left' ); ?>
             <?php get_template_part( 'components/page/content', 'front-page-2cols-right' ); ?>
+            <?php get_template_part( 'components/page/content', 'front-page-3cols' ); ?>
+            <?php get_template_part( 'components/page/content', 'front-page-4cols' ); ?>
 
         </div><!-- #content -->
     </div><!-- #primary -->

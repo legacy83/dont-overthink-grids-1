bc75f78d53017f7d7ca8f07bd2cfb7e8ad213288
Thiago Senna <trsenna@gmail.com>
2016-10-15 20:22:14 -0300

    added col-wrap

--- components/page/content-front-page-2cols.php
+++ components/page/content-front-page-2cols.php
@@ -7,12 +7,14 @@
         <p>Cras ornare nulla quis lobortis pellentesque. Aenean hendrerit, orci ut consequat commodo, orci lorem viverra
             est, non pharetra tortor felis eu metus. Integer interdum libero sapien.</p>
 
-        <div class="col">
-            <h4>.col</h4>
-        </div>
+        <div class="col-wrap">
+            <div class="col">
+                <h4>.col</h4>
+            </div>
 
-        <div class="col">
-            <h4>.col</h4>
+            <div class="col">
+                <h4>.col</h4>
+            </div>
         </div>
     </div><!-- .entry-content -->
 </section><!-- .front-page-2cols -->
--- components/page/content-front-page-3cols.php
+++ components/page/content-front-page-3cols.php
@@ -6,17 +6,19 @@
     <div class="entry-content">
         <p>Cras ornare nulla quis lobortis pellentesque. Aenean hendrerit, orci ut consequat commodo, orci lorem viverra
             est, non pharetra tortor felis eu metus. Integer interdum libero sapien.</p>
-        
-        <div class="col">
-            <h4>.col</h4>
-        </div>
 
-        <div class="col">
-            <h4>.col</h4>
-        </div>
+        <div class="col-wrap">
+            <div class="col">
+                <h4>.col</h4>
+            </div>
 
-        <div class="col">
-            <h4>.col</h4>
+            <div class="col">
+                <h4>.col</h4>
+            </div>
+
+            <div class="col">
+                <h4>.col</h4>
+            </div>
         </div>
     </div><!-- .entry-content -->
 </section><!-- .front-page-3cols -->

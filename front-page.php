38cfc7669bdd79b21728c8bab27ea297a9ce1a1a
Thiago Senna <trsenna@gmail.com>
2016-10-15 19:43:19 -0300

    added 2col-left/right

--- front-page.php
+++ front-page.php
@@ -7,6 +7,8 @@
             <?php get_template_part( 'components/page/content', 'front-page-2cols' ); ?>
             <?php get_template_part( 'components/page/content', 'front-page-3cols' ); ?>
             <?php get_template_part( 'components/page/content', 'front-page-4cols' ); ?>
+            <?php get_template_part( 'components/page/content', 'front-page-2cols-left' ); ?>
+            <?php get_template_part( 'components/page/content', 'front-page-2cols-right' ); ?>
 
         </div><!-- #content -->
     </div><!-- #primary -->

22428535ff5dc3263240aa181de0baafdb42f639
Thiago Senna <trsenna@gmail.com>
2016-10-16 23:59:05 -0200

    better styling functionality

--- includes/functions-styles.php
+++ includes/functions-styles.php
@@ -7,21 +7,17 @@
  */
 namespace Dont\Overthink\Grids\Theme;
 
-// register theme styles
-add_action( 'wp_enqueue_scripts', function () {
-    $template_dir_uri = trailingslashit( get_template_directory_uri() );
-    wp_register_style( 'twenty12-fonts', esc_url_raw( twentytwelve_get_font_url() ) );
-    wp_register_style( 'twenty12-parent', "{$template_dir_uri}style.css" );
-    wp_register_style( 'twenty12-style', get_stylesheet_uri(), array(
-        'twenty12-fonts',
-        'twenty12-parent',
-    ) );
-} );
-
-// enqueue theme styles
+// dequeue parent theme styles
 add_action( 'wp_enqueue_scripts', function () {
     wp_dequeue_style( 'twentytwelve-fonts' );
     wp_dequeue_style( 'twentytwelve-style' );
     wp_dequeue_style( 'twentytwelve-ie' );
-    wp_enqueue_style( 'twenty12-style' );
 }, 15 );
+
+// enqueue theme styles
+add_action( 'wp_enqueue_scripts', function () {
+    $template_dir_uri = trailingslashit( get_template_directory_uri() );
+    wp_enqueue_style( 'dont-overthink-grids-fonts', esc_url_raw( twentytwelve_get_font_url() ) );
+    wp_enqueue_style( 'dont-overthink-grids-parent', "{$template_dir_uri}style.css" );
+    wp_enqueue_style( 'dont-overthink-grids-style', get_stylesheet_uri() );
+} );

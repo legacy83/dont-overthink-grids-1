bd7f71804b86ec25b2ab777bfe5a7ae1830b3679
Thiago Senna <trsenna@gmail.com>
2018-09-14 22:00:27 -0300

    bootstrap-compat, saving progress

--- functions.php
+++ functions.php
@@ -1,6 +1,17 @@
 <?php
 
 # ------------------------------------------------------------------------------
+# Compatibility check.
+# ------------------------------------------------------------------------------
+#
+# Check that the site meets the minimum requirements for the theme before
+# proceeding.
+#
+
+$safe = require_once( get_theme_file_path( 'includes/bootstrap-compat.php' ) );
+if ( !$safe ) return;
+
+# ------------------------------------------------------------------------------
 # Bootstrap the theme.
 # ------------------------------------------------------------------------------
 #
@@ -8,7 +19,5 @@
 # any classes/functions are available that we might need.
 #
 
-if ( defined( 'DALEN_THE_PLUGIN' ) && DALEN_THE_PLUGIN ) {
-    require_once( get_theme_file_path( 'includes/bootstrap-autoload.php' ) );
-    require_once( get_theme_file_path( 'includes/bootstrap-theme.php' ) );
-}
+require_once( get_theme_file_path( 'includes/bootstrap-autoload.php' ) );
+require_once( get_theme_file_path( 'includes/bootstrap-theme.php' ) );
---
+++ includes/bootstrap-compat.php
@@ -0,0 +1,3 @@
+<?php
+
+return defined( 'DALEN_THE_PLUGIN' ) && DALEN_THE_PLUGIN;

aa182c8d070788c250256acabb15ce7e11182202
Thiago Senna <trsenna@gmail.com>
2018-09-15 13:07:19 -0300

    added all sections

--- front-page.php
+++ front-page.php
@@ -3,50 +3,60 @@
 namespace site\dogrids;
 
 add_action( 'site/dogrids/front-page/content', function () {
+
+    $section = new \stdClass();
+    $section->title = __( "Don't Overthink Grids", 'dogrids' );
+    $section->classes = [ 'front-page-intro' ];
+
     echo template( 'partials/front-page/front-page-section', 'intro' )->render( [
-        'title' => __( "Don't Overthink Grids", 'dogrids' ),
-        'classes' => [ 'front-page-intro' ],
+        'title' => $section->title,
+        'classes' => $section->classes,
     ] );
+
 } );
 
 add_action( 'site/dogrids/front-page/content', function () {
-    echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
-        'title' => __( "Two Columns", 'dogrids' ),
-        'columns' => 2,
-        'classes' => [ 'front-page-2cols' ],
-    ] );
-} );
 
-add_action( 'site/dogrids/front-page/content', function () {
-    echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
-        'title' => __( "Two Columns - Left", 'dogrids' ),
-        'columns' => 2,
-        'classes' => [ 'front-page-2cols-left' ],
-    ] );
-} );
+    $sections = [];
 
-add_action( 'site/dogrids/front-page/content', function () {
-    echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
-        'title' => __( "Two Columns - Right", 'dogrids' ),
-        'columns' => 2,
-        'classes' => [ 'front-page-2cols-right' ],
-    ] );
-} );
+    $index = sizeof( $sections );
+    $sections[ $index ] = new \stdClass();
+    $sections[ $index ]->title = __( "Two Columns", 'dogrids' );
+    $sections[ $index ]->columns = 2;
+    $sections[ $index ]->classes = [ 'front-page-2cols' ];
 
-add_action( 'site/dogrids/front-page/content', function () {
-    echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
-        'title' => __( "Three Columns", 'dogrids' ),
-        'columns' => 3,
-        'classes' => [ 'front-page-3cols' ],
-    ] );
-} );
+    $index = sizeof( $sections );
+    $sections[ $index ] = new \stdClass();
+    $sections[ $index ]->title = __( "Two Columns - Left", 'dogrids' );
+    $sections[ $index ]->columns = 2;
+    $sections[ $index ]->classes = [ 'front-page-2cols-left' ];
 
-add_action( 'site/dogrids/front-page/content', function () {
-    echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
-        'title' => __( "Four Columns", 'dogrids' ),
-        'columns' => 4,
-        'classes' => [ 'front-page-4cols' ],
-    ] );
+    $index = sizeof( $sections );
+    $sections[ $index ] = new \stdClass();
+    $sections[ $index ]->title = __( "Two Columns - Right", 'dogrids' );
+    $sections[ $index ]->columns = 2;
+    $sections[ $index ]->classes = [ 'front-page-2cols-right' ];
+
+    $index = sizeof( $sections );
+    $sections[ $index ] = new \stdClass();
+    $sections[ $index ]->title = __( "Three Columns", 'dogrids' );
+    $sections[ $index ]->columns = 3;
+    $sections[ $index ]->classes = [ 'front-page-3cols' ];
+
+    $index = sizeof( $sections );
+    $sections[ $index ] = new \stdClass();
+    $sections[ $index ]->title = __( "Four Columns", 'dogrids' );
+    $sections[ $index ]->columns = 4;
+    $sections[ $index ]->classes = [ 'front-page-4cols' ];
+
+    foreach ( $sections as $section ) {
+        echo template( 'partials/front-page/front-page-section', 'cols' )->render( [
+            'title' => $section->title,
+            'columns' => $section->columns,
+            'classes' => $section->classes,
+        ] );
+    }
+
 } );
 
 echo template( 'front-page' )->render();

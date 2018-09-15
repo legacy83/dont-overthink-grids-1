6529c482f10c7ee7efb7486191938d88107d9738
Thiago Senna <trsenna@gmail.com>
2018-09-14 23:19:26 -0300

    bootstrap-compat, saving progress

--- includes/bootstrap-compat.php
+++ includes/bootstrap-compat.php
@@ -1,8 +1,40 @@
 <?php
 
+# ------------------------------------------------------------------------------
+# Minimal WP/PHP requirements.
+# ------------------------------------------------------------------------------
+#
+# This section checks if PHP and WordPress has their minimal
+# requirements satisfied.
+#
+
 $safe = version_compare( $GLOBALS[ 'wp_version' ], '4.9', '>=' );
 $safe = $safe && version_compare( PHP_VERSION, '5.6', '>=' );
+
+# ------------------------------------------------------------------------------
+# Searches for `Dalen-the-plugin` plugin.
+# ------------------------------------------------------------------------------
+#
+# This section checks if `Dalen-the-plugin` plugin is active. Also
+# checks if it's minimal version is satisfied.
+#
+
 $safe = $safe && defined( 'DALEN_THE_PLUGIN' ) && DALEN_THE_PLUGIN;
-$safe = $safe && version_compare( DALEN_THE_PLUGIN_VERSION, '0.2.0', '>=' );
+$safe = $safe && version_compare( DALEN_THE_PLUGIN_VERSION, '0.3.0', '>=' );
+
+# ------------------------------------------------------------------------------
+# Maybe display error messages.
+# ------------------------------------------------------------------------------
+#
+# Maybe display an error message based on wether minimum requirements
+# wasn't met.
+#
+
+if ( !$safe ) {
+    add_action( 'admin_notices', function () {
+        $message = __( 'Minimal requirements are not satisfied by active theme. Please, ask for some help from your developer or just activate another theme.', 'dogrids' );
+        printf( '<div class="error"><p>%s</p></div>', $message );
+    } );
+}
 
 return $safe;

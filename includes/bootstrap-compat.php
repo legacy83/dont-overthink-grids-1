3e97d9ecdbf0881e2a90bbc00ea1f81e14bdf022
Thiago Senna <trsenna@gmail.com>
2018-09-14 22:22:07 -0300

    bootstrap-compat, saving progress

--- includes/bootstrap-compat.php
+++ includes/bootstrap-compat.php
@@ -1,3 +1,8 @@
 <?php
 
-return defined( 'DALEN_THE_PLUGIN' ) && DALEN_THE_PLUGIN;
+$safe = version_compare( $GLOBALS[ 'wp_version' ], '4.9', '>=' );
+$safe = $safe && version_compare( PHP_VERSION, '5.6', '>=' );
+$safe = $safe && defined( 'DALEN_THE_PLUGIN' ) && DALEN_THE_PLUGIN;
+$safe = $safe && version_compare( DALEN_THE_PLUGIN_VERSION, '0.2.0', '>=' );
+
+return $safe;

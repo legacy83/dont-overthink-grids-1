8e065be9e7055cd2bd2c1f88db91da01c5443422
Thiago Senna <trsenna@gmail.com>
2018-09-17 01:15:34 -0300

    some minor testing - wicket as reference

---
+++ includes/src/domain/shared/DataProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace site\dogrids\domain\shared;
+
+/**
+ * Interface DataProvider
+ *
+ * @package site\dogrids\domain\shared
+ */
+interface DataProvider extends \IteratorAggregate
+{
+
+}
---
+++ includes/src/domain/shared/ModelInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace site\dogrids\domain\shared;
+
+/**
+ * Interface ModelInterface
+ *
+ * @package site\dogrids\domain\shared
+ */
+interface ModelInterface
+{
+    /**
+     * Get the model object.
+     *
+     * @return object
+     */
+    public function getObject();
+}

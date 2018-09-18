d728e05f72008dc54cfb8831baba225280c6e522
Thiago Senna <trsenna@gmail.com>
2018-09-18 10:08:55 -0300

    repeater classes added, very simple now

--- front-page.php
+++ front-page.php
@@ -2,14 +2,12 @@
 
 namespace site\dogrids;
 
+use site\dogrids\domain\ColumnsRepeater;
+use site\dogrids\domain\IntroRepeater;
+
 add_action( 'site/dogrids/front-page/content', function () {
 
-    $repeater = new \ArrayIterator( [
-        [
-            'title' => "Don't Overthink Grids",
-            'classes' => [ 'front-page-intro' ],
-        ]
-    ] );
+    $repeater = component( IntroRepeater::class );
 
     foreach ( $repeater as $intro ) {
         echo template( 'partials/front-page/front-page-section', 'intro' )->render( [
@@ -21,33 +19,7 @@ add_action( 'site/dogrids/front-page/content', function () {
 
 add_action( 'site/dogrids/front-page/content', function () {
 
-    $repeater = new \ArrayIterator( [
-        [
-            'size' => 2,
-            'title' => 'Two Columns',
-            'classes' => [ 'front-page-2cols' ]
-        ],
-        [
-            'size' => 2,
-            'title' => 'Two Columns - Left',
-            'classes' => [ 'front-page-2cols-left' ]
-        ],
-        [
-            'size' => 2,
-            'title' => 'Two Columns - Right',
-            'classes' => [ 'front-page-2cols-right' ]
-        ],
-        [
-            'size' => 3,
-            'title' => 'Three Columns',
-            'classes' => [ 'front-page-3cols' ]
-        ],
-        [
-            'size' => 4,
-            'title' => 'Four Columns',
-            'classes' => [ 'front-page-4cols' ]
-        ],
-    ] );
+    $repeater = component( ColumnsRepeater::class );
 
     foreach ( $repeater as $columns ) {
         echo template( 'partials/front-page/front-page-section', 'columns' )->render( [

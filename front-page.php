8dddc3ffbf86fc5b45e6511d6999c9a98ec0b671
Thiago Senna <trsenna@gmail.com>
2018-09-18 09:58:34 -0300

    repeater code moved to into the front-page template

--- front-page.php
+++ front-page.php
@@ -2,12 +2,16 @@
 
 namespace site\dogrids;
 
-use site\dogrids\domain\ColumnsDataProvider;
-use site\dogrids\domain\IntroModel;
-
 add_action( 'site/dogrids/front-page/content', function () {
 
-    foreach ( component( IntroModel::class ) as $intro ) {
+    $repeater = new \ArrayIterator( [
+        [
+            'title' => "Don't Overthink Grids",
+            'classes' => [ 'front-page-intro' ],
+        ]
+    ] );
+
+    foreach ( $repeater as $intro ) {
         echo template( 'partials/front-page/front-page-section', 'intro' )->render( [
             'intro' => (object)$intro,
         ] );
@@ -17,7 +21,35 @@ add_action( 'site/dogrids/front-page/content', function () {
 
 add_action( 'site/dogrids/front-page/content', function () {
 
-    foreach ( component( ColumnsDataProvider::class ) as $columns ) {
+    $repeater = new \ArrayIterator( [
+        [
+            'size' => 2,
+            'title' => 'Two Columns',
+            'classes' => [ 'front-page-2cols' ]
+        ],
+        [
+            'size' => 2,
+            'title' => 'Two Columns - Left',
+            'classes' => [ 'front-page-2cols-left' ]
+        ],
+        [
+            'size' => 2,
+            'title' => 'Two Columns - Right',
+            'classes' => [ 'front-page-2cols-right' ]
+        ],
+        [
+            'size' => 3,
+            'title' => 'Three Columns',
+            'classes' => [ 'front-page-3cols' ]
+        ],
+        [
+            'size' => 4,
+            'title' => 'Four Columns',
+            'classes' => [ 'front-page-4cols' ]
+        ],
+    ] );
+
+    foreach ( $repeater as $columns ) {
         echo template( 'partials/front-page/front-page-section', 'columns' )->render( [
             'columns' => (object)$columns,
         ] );

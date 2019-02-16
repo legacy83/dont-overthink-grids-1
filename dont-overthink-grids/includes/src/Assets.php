ecf88af74cc544eeb0d05e6688a296c96106409d
Thiago Senna <trsenna@gmail.com>
2019-02-16 19:16:27 -0200

    some updates

--- dont-overthink-grids/includes/src/Assets.php
+++ dont-overthink-grids/includes/src/Assets.php
@@ -21,16 +21,7 @@ class Assets implements BootstrapInterface, ServiceProviderInterface
     public function __bootstrap()
     {
         add_action( 'wp_enqueue_scripts', function () {
-            wp_dequeue_style( 'twentytwelve-fonts' );
-            wp_dequeue_style( 'twentytwelve-style' );
-            wp_dequeue_style( 'twentytwelve-ie' );
-        }, 15 );
-
-        add_action( 'wp_enqueue_scripts', function () {
-            $template_dir_uri = trailingslashit( get_template_directory_uri() );
-            wp_enqueue_style( 'dont-overthink-grids-fonts', esc_url_raw( twentytwelve_get_font_url() ) );
-            wp_enqueue_style( 'dont-overthink-grids-parent', "{$template_dir_uri}style.css" );
-            wp_enqueue_style( 'dont-overthink-grids-style', get_stylesheet_uri() );
+            wp_enqueue_style( 'dogrids-style', get_stylesheet_uri() );
         } );
     }
 }

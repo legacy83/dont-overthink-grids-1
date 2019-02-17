5335d29d7f73b0ac703fbe85e7626a8e31237909
Thiago Senna <trsenna@gmail.com>
2019-02-17 16:27:12 -0300

    saving cleanup

--- dont-overthink-grids/index.php
+++ dont-overthink-grids/index.php
@@ -2,30 +2,6 @@
 
 namespace site\dogrids;
 
-add_action( 'site/dogrids/front-page/content', function () {
-
-    $repeater = component( IntroRepeater::class );
-
-    foreach ( $repeater as $intro ) {
-        echo template( 'partials/front-page/front-page-section', 'intro' )->render( [
-            'intro' => $intro,
-        ] );
-    }
-
-} );
-
-add_action( 'site/dogrids/front-page/content', function () {
-
-    $repeater = component( ColumnsRepeater::class );
-
-    foreach ( $repeater as $columns ) {
-        echo template( 'partials/front-page/front-page-section', 'columns' )->render( [
-            'columns' => $columns,
-        ] );
-    }
-
-} );
-
 component( Markup::class )
     ->layout( 'layout/main/main' )
     ->content( 'content-page/page/page', 'front' );
